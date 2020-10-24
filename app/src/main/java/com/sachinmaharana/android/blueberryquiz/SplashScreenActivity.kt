b793ad388afd9cd2ab641d32d648ff27f05d777d
SachinMaharana <sachin.nicky@gmail.com>
2020-10-24 19:47:21 +0530

    Added Font, SplashScreen

---
+++ app/src/main/java/com/sachinmaharana/android/blueberryquiz/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.sachinmaharana.android.blueberryquiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
